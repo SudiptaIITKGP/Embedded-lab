79e7ad383907f77e22b9234059eea2fbb041eab1
Sudipta Mandal <sudipta.m1997@gmail.com>
2018-01-22 22:48:59 +0530

    Create Resistance_on_LED.ino

---
+++ Exp 3/Resistance_on_LED.ino
@@ -0,0 +1,36 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(8,9,4,5,6,7);
+
+int analogPin= A1;
+int raw= 0;
+int Vin= 5;
+float Vout= 0;
+float R1= 1500;
+float R2= 0;
+float buffer= 0;
+
+void setup()
+{
+lcd.begin(16,2);
+lcd.clear(); 
+//Serial.begin(9600);
+
+}
+
+void loop()
+{
+raw= analogRead(analogPin);
+if(raw) 
+{
+buffer= raw * Vin;
+Vout= (buffer)/1024.0;
+buffer= (Vin/Vout) -1;
+R2= R1 * buffer;
+//Serial.print("R2: ");
+//Serial.println(R2);
+lcd.setCursor(0,0);
+lcd.print(R2);
+
+}
+}
